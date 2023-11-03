c561755af29a3d0c569855a05034b585aafbb5ff
Ahmed-Sharkawy <ahmedmaher0110@gmail.com>
2023-11-03 20:27:47 +0200

    fix bug

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -36,5 +38,15 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+
+    public function login(Request $request)
+    {
+        if (Auth::attempt(['phone' => $request->phone, 'password' => $request->password, 'admin' => 1])) {
+            return redirect()->route('home');
+        }
+
+        return redirect()->route('login')->with('error', 'phone or passowrd not valid');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\Auth\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,9 +23,13 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('user/create', [UserController::class, 'create'])->name('user.create');
-Route::post('user/store', [UserController::class, 'store'])->name('user.store');
-Route::get('user/edit/{user}', [UserController::class, 'edit'])->name('user.edit');
-Route::put('user/update/{user}', [UserController::class, 'update'])->name('user.update');
-Route::get('user/delete/{user}', [UserController::class, 'destroy'])->name('user.destroy');
+Route::post('login', [LoginController::class, 'login'])->name('login');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    Route::get('user/create', [UserController::class, 'create'])->name('user.create');
+    Route::post('user/store', [UserController::class, 'store'])->name('user.store');
+    Route::get('user/edit/{user}', [UserController::class, 'edit'])->name('user.edit');
+    Route::put('user/update/{user}', [UserController::class, 'update'])->name('user.update');
+    Route::get('user/delete/{user}', [UserController::class, 'destroy'])->name('user.destroy');
+});
